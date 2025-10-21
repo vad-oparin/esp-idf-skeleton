421dcd4822f50e04fe263bc7903e0d0d5c78a0ad
Vadim Oparin <vad.oparin@gmail.com>
2025-10-21 14:11:03 +0300

    Fix test build with app_main and unity runner

--- test/main/test_app.c
+++ test/main/test_app.c
@@ -1,6 +1,12 @@
 #include "unity.h"
+#include "unity_test_runner.h"
 
 TEST_CASE("Template test", "[template]")
 {
     TEST_ASSERT_EQUAL(1, 1);
+}
+
+void app_main(void)
+{
+    unity_run_menu();
 }
